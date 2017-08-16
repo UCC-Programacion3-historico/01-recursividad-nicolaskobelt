a8c1b00968f2416bc602428a53021e2deeb607ee
nicolaskobelt <kobeltnicolas@me.com>
2017-08-16 11:56:19 -0300

    MCD OK

--- 04_MCD/mcd.cpp
+++ 04_MCD/mcd.cpp
@@ -1,5 +1,15 @@
 #include "mcd.h"
 
 unsigned int mcd(unsigned int a, unsigned int b) {
-    return 0;
+    if (b==0)
+        return a;
+
+    return mcd(b,a%b);
 }
+
+/*Algoritmo de Euclides: mostrar que el máximo común divisor (mcd) de a y b,
+ * (a > b > 0), es igual a a si b es cero, en otro caso es igual al mcd de b y
+ * el remanente de a dividido por b, si b > 0.
+ *
+ *
+ */
