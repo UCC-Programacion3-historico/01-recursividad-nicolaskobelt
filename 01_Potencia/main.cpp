63b77aa6237e2eb34e91f46befafd942e7527523
nicolaskobelt <kobeltnicolas@me.com>
2017-08-16 10:20:49 -0300

    Potencia OK

--- 01_Potencia/main.cpp
+++ 01_Potencia/main.cpp
@@ -2,6 +2,7 @@
 #include "potencia.h"
 
 int main() {
+    potencia(3,4);
     std::cout << "Ejercicio 01/01\n" << std::endl;
     return 0;
 }
--- 01_Potencia/potencia.cpp
+++ 01_Potencia/potencia.cpp
@@ -1,4 +1,9 @@
-
+#include "potencia.h"
 unsigned int potencia(unsigned int base, unsigned int exp) {
-    return 0;
+
+    if(exp==0)
+        return 1;
+    unsigned int ant = potencia(base,exp-1);
+    return base*ant;
 }
+
