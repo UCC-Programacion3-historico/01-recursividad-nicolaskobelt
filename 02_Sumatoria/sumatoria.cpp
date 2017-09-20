e888ef1c995f9360adb470240101e5db202e16ac
nicolaskobelt <kobeltnicolas@me.com>
2017-09-20 09:29:21 -0300

    Repaso

--- 02_Sumatoria/sumatoria.cpp
+++ 02_Sumatoria/sumatoria.cpp
@@ -1,9 +1,11 @@
 #include "sumatoria.h"
-
+#include <iostream>
+using namespace std;
 
 int sumatoria(int *arr, unsigned int size) {
     if (size==0)
         return 0;
-
+    
+    cout<<"Valor previo: "<<arr[size-1]+sumatoria(arr,size-1)<<endl;
     return arr[size-1]+sumatoria(arr,size-1);
 }
